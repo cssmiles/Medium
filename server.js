434cfa444d356450463694fc6bc713abbe7484ff
CSP <cssmiles@gmail.com>
2021-01-02 21:05:36 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const { ApolloServer, gql } = require("apollo-server");
+const { unmarshall } = require("@aws-sdk/util-dynamodb");
+const { DynamoDBClient, ScanCommand } = require("@aws-sdk/client-dynamodb");
+
+const client = new DynamoDBClient({ region: "us-east-1" });
+
+const getQuotes = async () => {
+  const params = {
+    TableName: "Quote",
+  };
+
+  try {
+    const results = await client.send(new ScanCommand(params));
+    const quotes = [];
+    results.Items.forEach((item) => {
+      quotes.push(unmarshall(item));
+    });
+    return quotes;
+  } catch (err) {
+    console.error(err);
+    return err;
+  }
+};
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  scalar JSON
+  type Quote {
+    id: ID!
+    value: String
+    source: String
+  }
+  type Query {
+    quotes: [Quote]
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    quotes: () => {
+      return getQuotes();
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
